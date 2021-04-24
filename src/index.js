c627a82f4478227c121d41f9f5cf78acd7ab9ccf
Chris Williamson <chriswill0629@gmail.com>
2021-04-24 09:47:51 -0400

    adding in javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 //import pageLoad from './modules/pageLoad.js'
+
+let projectTitles = {
+    'My project One': [{
+        checked: false,
+        desc: "My Description about this note is null",
+        dueDate: "06/26/2021",
+        noteName: "My Note 1"
+    }]
+};
+
+let selectedNote = '';
---
+++ src/modules/storage.js
@@ -0,0 +1,9 @@
+// Put the object into storage
+localStorage.setItem('projectTitles', JSON.stringify(projectTitles));
+
+// Retrieve the object from storage
+var retrievedObject = localStorage.getItem('projectTitles');
+
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+let keys2 = Object.keys(JSON.parse(retrievedObject))
